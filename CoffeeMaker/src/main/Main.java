832946ac511da4dacaae4add74ba401241b67b48
DevJ5 <idevelopjs@gmail.com>
2020-04-28 16:39:58 +0200

    Exception tests

--- CoffeeMaker/src/main/Main.java
+++ CoffeeMaker/src/main/Main.java
@@ -13,6 +13,7 @@ public class Main {
         try {
             coffeeMaker.brew(2.4, 50);
             coffeeMaker.pourCoffee();
+            assert(coffeeMaker.getCupsRemaining() == 19);
         } catch (BeansAmountException beansException) {
             System.out.println(beansException.getMessage());
         } catch (WaterException waterException) {
