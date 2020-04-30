122f2655eeb2b97567396945a18ec69d4d57e3d9
DevJ5 <idevelopjs@gmail.com>
2020-04-30 14:32:51 +0200

    Implementing OOP Design

---
+++ PetPairsStarter/src/model/PetStore.java
@@ -0,0 +1,40 @@
+package model;
+
+import model.pets.Pet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PetStore {
+    private HashMap<String, ArrayList<Pet>> animals = new HashMap<>();
+
+    //MODIFIES: this
+    //EFFECTS: adds p to the petstore
+    public void addPet(Pet p){
+        ArrayList<Pet> pets = animals.get(p.getSpecies());
+        if (pets==null) {
+            pets = new ArrayList<>();
+        }
+        pets.add(p);
+        animals.put(p.getSpecies(), pets);
+    }
+
+    //EFFECTS: finds a pet matching this species and color, if it exists in the store
+    public Pet findMatchingPet(String species, String color){
+        ArrayList<Pet> pets = animals.get(species);
+
+        if (pets==null){
+            return null;
+        }
+
+        Pet p = new Pet(species, color, 0.00);
+
+        if (pets.contains(p)){
+            int i = pets.indexOf(p);
+            return pets.get(i);
+        }
+        return null;
+    }
+
+}
